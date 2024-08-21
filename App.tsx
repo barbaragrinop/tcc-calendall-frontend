7963eb0b35db40bb5bb7a189d1bab304a1efb36a
barbaragrinop <barbarapereira123@hotmail.com>
2024-08-20 23:43:21 -0300

    feature: add status bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
-import Home from './src/screens/Home';
+import { StatusBar } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
-  return <Home />;
+	return (
+		<>
+			<StatusBar barStyle={"light-content"}  />
+			<Home />
+		</>
+	);
 }
-
