7cd8e019a275f2996fe70a3db7afaa8bb00568eb
BarbaraHellen <barbara.hellen@icarotech.com>
2024-10-28 01:32:29 -0300

    feature: adiciona tela de salas

--- screens/Classroom/index.tsx
+++ screens/Classroom/index.tsx
@@ -1,28 +1,129 @@
-import { Input, LogoText } from "@/components";
+import { Header, Input, LogoText } from "@/components";
 import * as S from "./style";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import { faChevronRight, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { Classroom } from "@/components";
+import { FlatList, GestureResponderEvent, TouchableOpacity } from "react-native";
+import { Classroom as TClassroom, Priority } from "@/types";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const mockClassRooms: TClassroom[] = [
+    {
+        title: "Sala de Redes asd asd asd asd asd as",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "1",
+    },
+    {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: false,
+        idClassroom: "2",
+    }, {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "3",
+    }, {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "4",
+    }, {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "5",
+    }, {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "6",
+    }, {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "7",
+    }, {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "8",
+    },
+    {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "9",
+    },
+    {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "10",
+    },
+    {
+        title: "Sala de Redes",
+        eventsQuantity: 3,
+        membersQuantity: 10,
+        isAdmin: true,
+        idClassroom: "11",
+    },
+]
 
 export function ClassroomScreen() {
+
+    function handleClick(event: GestureResponderEvent) {
+        console.log("asdljkas da ok")
+    }
+
     return (
         <S.Wrap>
-            <S.LogoSpace>
-                <S.CalendallText>
-                    <LogoText color="#fff" size={33} />
-                </S.CalendallText>
-                <S.Image
-                    source={require("../../assets/images/profile-nophoto.png")}
-                    style={{ width: 30, height: 30 }}
-                />
-            </S.LogoSpace>
-            <S.Content>
-                <S.TitleCreate>
-                    <S.TitleScreen>SALAS</S.TitleScreen>
-                    <S.ButtonCreateClassroom>
-                        <S.TitleCreateClassroom>criar sala</S.TitleCreateClassroom>
-                    </S.ButtonCreateClassroom>  
-                </S.TitleCreate>
-                <Input label="pesquise pelo nome da sala" placeholder="pesquise pelo nome da sala " required />
-            </S.Content>
-            
+            <Header.ProfileInfo />
+            <SafeAreaProvider>
+                <S.Container>
+                    <S.TitleCreate>
+                        <S.TitleScreen>SALAS</S.TitleScreen>
+                        <S.ButtonCreateClassroom>
+                            <S.TitleCreateClassroom>criar sala</S.TitleCreateClassroom>
+                        </S.ButtonCreateClassroom>
+                    </S.TitleCreate>
+                    <S.Content>
+                        <Input
+                            icon={<FontAwesomeIcon icon={faMagnifyingGlass} color="white" size={20} />}
+                            placeholder="pesquise pelo nome da sala"
+                        />
+                        <S.ClassroomList>
+                            <FlatList
+                                data={mockClassRooms}
+                                renderItem={({ item, index }) => (
+                                    <TouchableOpacity onPress={handleClick}>
+                                        <Classroom
+                                            isAdmin={item.isAdmin}
+                                            title={item.title}
+                                            eventsQuantity={item.eventsQuantity}
+                                            membersQuantity={item.membersQuantity}
+                                            idClassroom={(index + 1).toString()}
+                                        />
+                                    </TouchableOpacity>
+                                )}
+                                keyExtractor={item => item.idClassroom}
+                            />
+                        </S.ClassroomList>
+                    </S.Content>
+                </S.Container>
+            </SafeAreaProvider>
         </S.Wrap>
     );
 }
